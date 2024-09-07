6b3dd64b01e02a3ee5acaa3a80117bdb2b097ac7
Asus <semyanovandrew@yandex.ru>
2024-09-07 18:26:53 +0300

    added quicksort

--- main.py
+++ main.py
@@ -165,7 +165,8 @@ def theory_algoritms_topics(message):
     markup = telebot.types.InlineKeyboardMarkup(row_width=2)
     btn1 = telebot.types.InlineKeyboardButton("бинарный поиск", callback_data="бинарный поиск")
     btn2 = telebot.types.InlineKeyboardButton("сортировка выбором", callback_data="сортировка выбором")
-    markup.add(btn1, btn2)
+    btn3 = telebot.types.InlineKeyboardButton("быстрая сортировка", callback_data="быстрая сортировка")
+    markup.add(btn1, btn2, btn3)
     bot.send_message(message.chat.id, "Выберите тему", reply_markup=markup)
 
 
